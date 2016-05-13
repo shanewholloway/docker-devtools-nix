71cf2241e0ed3b0f52c599d70a09576b2886b731
Shane Holloway <shane.holloway@ieee.org>
2016-05-13 13:44:09 -0600

    Refactored user creation into a '/devtools/ensure-user' script. Added tini and gosu to devtools-nix:core. Changed some paths

--- build-all.sh
+++ build-all.sh
@@ -1,24 +1,24 @@
-#!/bin/sh
-echo ":: Building 'devtools-nix' chain ::\n" \
+#!/bin/bash
+echo -e ":: Building 'devtools-nix' chain ::\n" \
   \
-  && echo "\n\n:: Build 'devtools-nix:base' ::\n" \
+  && echo -e "\n\n:: Build 'devtools-nix:base' ::\n" \
   && docker build \
       -t shane/devtools-nix:base-xenial \
       -t shane/devtools-nix:base \
       ./devtools-base \
   \
-  && echo "\n\n:: Build 'devtools-nix:core' ::\n" \
+  && echo -e "\n\n:: Build 'devtools-nix:core' ::\n" \
   && docker build \
       -t shane/devtools-nix:core-xenial \
       -t shane/devtools-nix:core \
       -t shane/devtools-nix:latest \
       ./devtools-core \
   \
-  && echo "\n\n:: Build 'devtools-nix:example' ::\n" \
+  && echo -e "\n\n:: Build 'devtools-nix:example' ::\n" \
   && docker build \
       -t shane/devtools-nix:example-xenial \
       -t shane/devtools-nix:example \
       ./devtools-example \
   \
-  && echo "\n\n:: Completed building 'devtools-nix' chain ::\n" \
+  && echo -e "\n\n:: Completed building 'devtools-nix' chain ::\n" \
   && true
