1f92236894d9c486440660136629d6cae4dde773
Shane Holloway <shane.holloway@ieee.org>
2016-05-20 10:22:10 -0600

    Fixes and refinements.
    - Corrected initial documentation to be accurate to current reality.
    - Changed locale LC_ALL='C.UTF-8' from 'en_US.UTF-8', eliminating need for localedef calls.
    - Added cacerts for useful curl
    - Added parallel installation and building of devtools-nix:core

--- README.md
+++ README.md
@@ -6,13 +6,19 @@ DevOps tools using [Nix package manager](http://nixos.org/nix/)
 Make a named container using your `shane/devtools-nix` derived image:
 
     docker run --name my_devtools \
-      -v /devtools -v /nix
+      -v /devtools -v /nix \
       shane/devtools-nix:example
 
-Then compose your devtool container using `--volumes-from my_devtools` which shares `/devtools` between containers
+Then compose your devtool container using `--volumes-from my_devtools` which shares `/devtools` between containers:
 
     docker run --rm -it \
       --volumes-from my_devtools \
       ubuntu:xenial \
-      /nix/devtools/bin/node -p process.versions
+      /devtools/bin/node -p process.versions
+
+Or alternatively, using `docker-compose`:
+
+    docker-compose \
+      -f demo-docker-devtools-nix/docker-compose.yml \
+      run --rm demo
 
--- dkr-shell-utils.zsh
+++ dkr-shell-utils.zsh
@@ -1,14 +1,16 @@
 #!/usr/bin/env zsh
 
-dkr-cleanup() {
-  local containers=($(docker ps -q -f 'status=exited'))
+dkr-cleanup-containers() {
+  local containers=($(docker ps -qf 'status=exited'))
   if [[ -n "$containers" ]]; then
     echo "dkr-cleanup:: removing exited containers:"
     docker rm $containers
   else
     echo "dkr-cleanup:: zero exited containers"
   fi
+}
 
+dkr-cleanup-images() {
   local images=($(docker images -qf 'dangling=true'))
   if [[ -n "$images" ]]; then
     echo "dkr-cleanup:: removing dangling images:"
@@ -16,7 +18,9 @@ dkr-cleanup() {
   else
     echo "dkr-cleanup:: zero dangling images"
   fi
+}
 
+dkr-cleanup-volumes() {
   local volumes=($(docker volume ls -qf 'dangling=true'))
   if [[ -n "$volumes" ]]; then
     echo "dkr-cleanup:: removing dangling volumes:"
@@ -27,3 +31,12 @@ dkr-cleanup() {
   return 0
 }
 
+dkr-cleanup() {
+  dkr-cleanup-containers
+  dkr-cleanup-images
+  dkr-cleanup-volumes
+}
+
+dkr-images-flat() {
+  docker images --format "{{.Repository}}:{{.Tag}}"
+}
